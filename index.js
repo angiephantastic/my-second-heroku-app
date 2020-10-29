cf4c08e6ec8d8ecb407cb5c18c5b619fe02576cf
angiephantastic <nguyenphan1395@gmail.com>
2020-10-29 12:44:32 +0100

    wrong file previously added. Now fixed with port edited

--- index.js
+++ index.js
@@ -1,13 +1,64 @@
-const express = require("express")
-const app = express()
-const port = process.env.PORT || 3000
+require('dotenv').config()
 
-app.get("/", function(req, res){
+const express = require('express')
+const mysql = require('mysql2')
+
+const app = express()
+const port = process.env.PORT || 3306
+
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
+})
+
+app.get('/', function(req, res) {
+
+    console.log(`A requeste was madde to the home endpoint!`)
+
     res.json({
-        "Status": "Active"
+        status: 'ok',
+        description: `it's ok what else can I say...`
     })
 })
 
-app.listen(port, function(){
-    console.log(`Localhost: ${port}`)
+app.get('/dbtest', function(req, res) {
+
+    connection.query(`SELECT * FROM offices`, function(error, result) {
+    
+        res.json(result)
+    
+    })
+})
+
+app.get('/api/employees', function(req, res) {
+
+    connection.query(`SELECT * FROM employees`, function(error, result) {
+    
+        res.json(result)
+    
+    })
+})
+
+app.get('/api/employees/:id', function(req, res) {
+
+    const id = req.params.id
+
+    connection.query(`SELECT * FROM employees WHERE employeeNumber = ?`, [id], function(error, result) {
+        
+        if (error) {
+            res.json({
+                status: 'error',
+                description: `it's an error, try again later`
+            })
+        }
+
+        res.json(result)
+    
+    })
+})
+
+app.listen(port, function() {
+    console.log(`running on port ${port}`)
 })
